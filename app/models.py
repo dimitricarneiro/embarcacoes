bc91dee161a4b96b3874e806bb5a795e1d67925a
Dimitri Sean Ribeiro Carneiro <dimitri.carneiro@rfb.gov.br>
2025-02-03 15:44:57 -0300

    nome correto da tabela usuario

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class Usuario(db.Model, UserMixin):
-    __tablename__ = 'usuarios'
+    __tablename__ = 'usuario'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
