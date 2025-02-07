e1427e4ab5d4a5799f8850b0595eda34b78b8f5d
IvanBrasilico <brasilico.ivan@gmail.com>
2025-02-07 16:44:59 -0300

    Criando entrada para wsgi de produção

--- wsgi_production.py
+++ wsgi_production.py
@@ -1,8 +1,11 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
-from app import create_app, db
+from app import create_app
 
 app = create_app()
 
-application = DispatcherMiddleware(app)
-
+application = DispatcherMiddleware(app,
+                                   {
+                                       '/embarcacoes': app
+                                   }
+                                   )
